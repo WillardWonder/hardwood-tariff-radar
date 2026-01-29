ad560478247da5118fb0a842b4ea0fb1a2948330
WillardWonder <126423311+WillardWonder@users.noreply.github.com>
2026-01-29 15:35:29 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/hardwood-tariff-radar/',
-  build: {
-    outDir: 'dist',
-    sourcemap: true
-  }
+  base: '/hardwood-tariff-radar/'
 })
