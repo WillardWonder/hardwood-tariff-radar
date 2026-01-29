601fff7eb0b8d54ea8be5d2ea64056a512577e0e
WillardWonder <126423311+WillardWonder@users.noreply.github.com>
2026-01-29 08:53:02 -0600

    Initialize App component with TariffDashboard

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { TariffDashboard } from './components/TariffDashboard';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TariffDashboard />
+    </div>
+  );
+}
+
+export default App;
